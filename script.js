2e4a7d0c9eaea64c6e06507da8f8330bad3c4dee
Rishabh6666 <114308437+Rishabh6666@users.noreply.github.com>
2025-08-02 19:00:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Electronics and Computer Science Student. ','Software Developer.'],
+    strings:['Web Developer.','Front-End Developer.','Software Developer.'],
     typeSpeed:100,
     backSpeed:50,
     backDelay:1000,
